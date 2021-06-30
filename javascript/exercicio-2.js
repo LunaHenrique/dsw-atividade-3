b1b67dd8d3bacdf0563f76d5825b7f27d6aaf833
sarahgaby21 <86737041+sarahgaby21@users.noreply.github.com>
2021-06-30 17:09:32 -0300

    Exercicio-2 js

---
+++ javascript/exercicio-2.js
@@ -0,0 +1,5 @@
+function idade() {
+    var nome = document.getElementById("txtNome").value;
+    var idade = document.getElementById("txtIdade").value;
+    alert(nome+ " você tem "+idade+ " anos");
+}
